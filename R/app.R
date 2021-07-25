7235995ba444f37ff0ad1f868202b63eaac16c75
Kitt Burch <christopher.m.burch@gmail.com>
2021-07-25 17:55:17 -0400

    functioning two-chart shiny app

--- R/app.R
+++ R/app.R
@@ -28,9 +28,9 @@ ui <- dashboardPage(
       menuItem("Fleet Composition",
                tabName = 'composition',
                icon = icon('car')),
-      menuItem("Vehicle Locations",
-               tabName = 'vehicleMap',
-               icon = icon('map'))
+      menuItem("Fuel Use",
+               tabName = 'fuelUse',
+               icon = icon('gas-pump'))
     )
   ),
   
@@ -40,10 +40,19 @@ ui <- dashboardPage(
       # first tab
       tabItem(tabName = 'composition',
         fluidRow(
-          box(chorddiag::chorddiagOutput("inv_chord",
+          box(
+            div(
+              selectizeInput("states", label = "States",
+                          choices = state_abb,
+                          multiple = TRUE, 
+                          width = "70%",
+                          options = list(plugins = list('remove_button')))
+            ),
+            chorddiag::chorddiagOutput("inv_chord",
                                          width = "100%",
-                                         height = "500px"), 
-              width = 6),
+                                         height = "500px"),
+            width = 6,
+            title = "Count of Fleet Vehicles - Fuel and Vehicle Body Type"),
           column(width = 6,
                  valueBoxOutput("vehicle_count", width = NULL),
                  fluidRow(
@@ -51,32 +60,61 @@ ui <- dashboardPage(
                    valueBoxOutput("alt_count", width = 4),
                    valueBoxOutput("petro_count", width = 4)
                  ),
-                 fluidRow(
-                   box(width = 12,
-                       selectInput("states", label = "States",
-                                   choices = state_abb,
-                                   multiple = TRUE),
-                       actionButton("resetButton", "Clear Filter"))
-                 ),
-                 box(
-                   tagList(
-                     div("This chart displays information for 'on-road reportable' 
-              vehicles, a category which includes all non-industrial wheeled 
-              vehicles authorized to travel on public roadways."),
-                     tags$br(),
-                     div("This category does not include specialty vehicles used in 
-              construction (e.g., bucket loaders) or vehicles without an engine 
-              (e.g. trailers)."),
-                   ),
-                   title = "What types of vehicles are tracked?",
-                   width = NULL),
+                 box(div("Alternative Fuels are defined by the US Dept. of Energy, and 
+                          include Ethanol or E85, Biodiesel, Natural Gas, Propane, and 
+                          gas-powered Hybrid Electric vehicles."),
+                     title = "What Types of Fuel are Considered 'Alternative'?",
+                     width = NULL), 
+                 box(div("This chart displays information for 'on-road reportable' 
+                          vehicles, a category which includes all non-industrial wheeled 
+                          vehicles authorized to travel on public roadways. 
+                          All on-road reportable vehicles have license plates. 
+                          This category does not include specialty vehicles used in 
+                          construction (e.g., bucket loaders) or vehicles without an engine 
+                          (e.g. trailers)."),
+                     title = "What types of vehicles are tracked?",
+                     width = NULL)
           )
         )
       ),
       # second tab
-      tabItem(tabName = 'vehicleMap',
+      tabItem(tabName = 'fuelUse',
         fluidRow(
-          box(plotlyOutput("fleet_map"), width = 9),
+          box(plotly::plotlyOutput("fuel_sunburst",
+                                   width = "100%",
+                                   height = "500px"),
+              sliderInput("fuel_dates",
+                          "Filter by Date:",
+                          min = as.Date("2020-10-01"), # start of FY21
+                          max = max(fuel$date),        # most recent date
+                          value = c(as.Date("2020-10-01"), max(fuel$date)),
+                          timeFormat = "%Y-%m-%d", 
+                          width = NULL),
+              width = 6,
+              title = "Fuel Purchases in Gasoline Gallons Equivalent (GGE)"),
+          column(width = 6,
+                 valueBoxOutput("total_gge", width = NULL),
+                 fluidRow(
+                   valueBoxOutput("elec_gge", width = 4),
+                   valueBoxOutput("alt_gge", width = 4),
+                   valueBoxOutput("petro_gge", width = 4)
+                 ),
+                 box(div("Gasoline Gallon Equivalent (GGE) is a measure that relates 
+                         the energy content of alternative fuels with gasoline. 
+                         This allows us to compare the amount of fuel used between 
+                         conventional fuel vehicles and alternative fuel vehicles."),
+                     title = "What is GGE?",
+                     width = NULL), 
+                 box(div("This chart displays information for 'on-road reportable' 
+                          vehicles, a category which includes all non-industrial wheeled 
+                          vehicles authorized to travel on public roadways. 
+                          All on-road reportable vehicles have license plates. 
+                          This category does not include specialty vehicles used in 
+                          construction (e.g., bucket loaders) or vehicles without an engine 
+                          (e.g. trailers)."),
+                     title = "What types of vehicles are tracked?",
+                     width = NULL),
+          )
         )
       )
     )
@@ -86,8 +124,19 @@ ui <- dashboardPage(
 server <- function(input, output, session) {
   # load inventory data
   inv_data <- readr::read_rds("../data/fleet_inv.rds")
-  # load fuel data
-  fuel <- readr::read_rds("../data/fuel_by_state.rds")
+  # # load fuel data
+  isnt_out_mad <- function(x, thres = 3, na.rm = TRUE) {
+    abs(x - median(x, na.rm = na.rm)) <= thres * mad(x, na.rm = na.rm)
+  }
+  
+  fuel <- readRDS("../data/fur_2015_2021.rds") |> 
+    tidyr::drop_na(date) |> 
+    dplyr::filter(date >= as.Date("2020-10-01")) |> 
+    # remove negative values
+    dplyr::mutate(units = abs(units)) |> 
+    # remove outliers over 3sd away from median (MAD: Med Abs Dev)
+    dplyr::filter(isnt_out_mad(units))
+  
   # extract states
   states <- inv_data |> dplyr::distinct(garage_state)
   # specify chord colors
@@ -101,24 +150,34 @@ server <- function(input, output, session) {
     }
   })
   
-  # reset UI on button press
-  observeEvent(input$resetButton, {
-    updateSelectInput(session, "states", selected = "NULL")
-  })
+  # # reset UI on button press
+  # observeEvent(input$resetButton, {
+  #   updateSelectizeInput(session, "states", selected = "NULL")
+  # })
   
   # plot chord
   output$inv_chord <- chorddiag::renderChorddiag({
-    sel_inv() |> 
+    fuel_desc <- tibble(fuel_class = as.factor(c("HEV", "GAS", "FEV", "E85", "DSL", "B20/CNG")),
+                        class_desc = as.factor(c("Gas/Elec Hybrid",
+                                       "Gasoline",
+                                       "Electric",
+                                       "Ethanol (E85)",
+                                       "Diesel",
+                                       "Other\nFuel")))
+    # inv_data |> 
+    sel_inv() |>
       dplyr::select(vehicle_class, fuel_class) |> 
-      dplyr::mutate(across(.cols = everything(), ~ as.factor(.x))) |> 
-      with(table(vehicle_class, fuel_class,
+      dplyr::mutate(across(.cols = everything(), ~ as.factor(.x))) |>
+      dplyr::left_join(fuel_desc, by = 'fuel_class') |> 
+      with(table(vehicle_class, 
+                 class_desc,
                  dnn = list("Vehicle Type", "Fuel Type"))) |>
       chorddiag::chorddiag(type = "bipartite", 
                            groupnameFontsize = 14,
                            groupnamePadding = 4,
                            groupColors = group_cols,
-                           categorynameFontsize = 14,
-                           categorynamePadding = 50,
+                           categorynameFontsize = 16,
+                           categorynamePadding = 130,
                            showTicks = FALSE)
   })
   
@@ -155,55 +214,100 @@ server <- function(input, output, session) {
                                       col = "maroon",
                                       icon = icon("gas-pump"))
   
-  # plotly map options
-  g <- list(
-      scope = 'usa',
-      projection = list(type = 'albers usa'),
-      showlakes = FALSE,
-      lakecolor = toRGB('white')
-    )
-  
-  # plot vehicle map
-  us_states <- geojsonio::geojson_read("../json/gz_2010_us_040_00_5m.json", what = "sp")
-
-  output$fleet_map <- renderPlotly({
-    plot_geo({inv_data |>
-              select(state = garage_state) |>
-              count(state, name = "vehicles", sort = TRUE)},
-              locationmode = "USA-states") |>
-    add_trace(z = ~ vehicles,
-              locations = ~ state) |>
-    layout(geo = g,
-           title = paste(input$fuel_type, "Vehicle Count by State, FY2020"))
+  output$total_gge <- renderValueBox({
+    valueBox(value = format(sum(fuel_dat()$units, na.rm = TRUE), 
+                            big.mark = ",", 
+                            trim = TRUE, 
+                            scientific = FALSE,
+                            digits = 0),
+             subtitle = "Total Purchased GGEs",
+             col = "blue",
+             icon = icon('car'),
+             width = NULL)
   })
   
-  # plot fuel timeline
-  # output$fuel_timeline <- renderDygraph({
-  #   fuel_mx <-
-  #     fuel |> 
-  #     select(date = purchase_date, purchase_fuel, units) |>
-  #     group_by(date, purchase_fuel) |> 
-  #     summarize(units = sum(units), .groups = 'drop') |> 
-  #     pivot_wider(id_cols = date, names_from = purchase_fuel, values_from = units) |> 
-  #     select(date, E85, GAS, DSL)
-  #   
-  #   fuel_matrix <-
-  #     fuel_mx |> 
-  #     select(E85, GAS, DSL) |> 
-  #     xts(order.by = fuel_mx$date)
-  #   
-  #   dygraph(fuel_matrix) |> 
-  #     dySeries('GAS') |> 
-  #     dySeries('E85') |> 
-  #     dySeries('DSL') |> 
-  #     dyOptions(colors = RColorBrewer::brewer.pal(3, 'Set2'),
-  #               stepPlot = TRUE,
-  #               fillGraph = TRUE,
-  #               fillAlpha = 0.5,
-  #               drawGrid = FALSE) |> 
-  #     dyAxis('y', label = "Units (GGE)") |> 
-  #     dyRangeSelector(height = 20)
-  # })
+  render_fuel_box <- function(group, subtitle, col, icon = icon('car'), ...) {
+    renderValueBox({
+      valueBox(value = format(sum(filter(fuel_dat(), 
+                                         purchase_fuel %in% group)
+                                  $units, na.rm = TRUE),
+                              big.mark = ",", 
+                              trim = TRUE, 
+                              scientific = FALSE,
+                              ...),
+               subtitle = subtitle,
+               col = col,
+               icon = icon,
+               width = NULL)
+    })
+  }
+  
+  output$elec_gge <- render_fuel_box(group = c("ELE"),
+                                     subtitle = "GGE Electricity",
+                                     col = "green",
+                                     icon = icon('plug'),
+                                     digits = 2)
+  output$alt_gge <- render_fuel_box(group = c("E85", "B20", "CNG", "LPG"),
+                                    subtitle = "GGE Alternative Fuels",
+                                    col = "orange",
+                                    icon = icon('recycle'),
+                                    digits = 0)
+  output$petro_gge <- render_fuel_box(group = c("GAS", "DSL"),
+                                      subtitle = "GGE Petroleum",
+                                      col = "maroon",
+                                      icon = icon('gas-pump'),
+                                      digits = 0)
+
+  # sunburst
+  fuel_xwalk <- 
+    tibble::tibble(purchase_fuel = c("GAS", "E85", "DSL", 
+                                     "B20", "CNG", "LPG",
+                                     "ELE"),
+                   fuel_label = c("Unleaded<br>Gasoline", "Ethanol (E85)", "Diesel",
+                                  "BioDiesel (B20)", "Compressed<br>Natural Gas",
+                                  "Liquified<br>Petroleum Gas", "Electric"),
+                   fuel_class = c("Petroleum", "Alternative", "Petroleum",
+                                  "Alternative", "Alternative", "Alternative",
+                                  "Electric"))
+  
+  fuel_dat <- reactive({
+    fuel |> 
+      filter(date >= input$fuel_dates[1] & date <= input$fuel_dates)
+  })
+  
+  output$fuel_sunburst <- renderPlotly({
+    fuel_class_sum <- 
+      fuel_dat() |> 
+      select(date, purchase_fuel, units) |>
+      left_join(fuel_xwalk, by = 'purchase_fuel') |> 
+      group_by(purchase_fuel, fuel_class, fuel_label) |> 
+      summarise(units = sum(units, na.rm = TRUE), .groups = 'drop') |> 
+      filter(!is.na(purchase_fuel) & purchase_fuel != 'ELE') |> 
+      select(fuel_label, fuel_class, units) |> 
+      group_by(fuel_class) |> 
+      summarize(units = sum(units), .groups = 'drop') |> 
+      rename(fuel_label = fuel_class) |> 
+      mutate(fuel_class = "")
+    
+    fuel_plt <- 
+      fuel_dat() |> 
+      filter(date >= as.Date("2020-10-01")) |> 
+      select(date, purchase_fuel, units) |>
+      left_join(fuel_xwalk, by = 'purchase_fuel') |> 
+      group_by(purchase_fuel, fuel_class, fuel_label) |> 
+      summarise(units = sum(units, na.rm = TRUE), .groups = 'drop') |> 
+      filter(!is.na(purchase_fuel) & purchase_fuel != 'ELE') |> 
+      select(fuel_label, fuel_class, units)
+    
+    fuel_class_sum |> 
+      rbind(fuel_plt) |> 
+      plotly::plot_ly(
+        labels = ~ fuel_label,
+        parents = ~ fuel_class,
+        values = ~ round(units, 0),
+        type = 'sunburst'
+      )
+  })
   
 }
 shinyApp(ui, server)
